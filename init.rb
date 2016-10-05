7932866bd045fb183adb10ba316059c2cbd72cec
Sascha Graef <sg@davitec.de>
2016-10-05 09:50:39 +0200

    inital commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :forger_typo3 do
+  name 'Forger Typo3 plugin'
+  author 'Sascha Graef'
+  description 'This Plugin is for use with Redmine to represent the Typo3 Forge functianlity'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
