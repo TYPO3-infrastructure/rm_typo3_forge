e32ca4ee01c686971f245365b3fc901a8e1d7e6b
Sascha Graef <sg@davitec.de>
2016-10-18 13:14:18 +0200

    integrate source code patches of the old redmine

---
+++ lib/mailer_patch.rb
@@ -0,0 +1,25 @@
+module MailerPatch
+  def self.included(base) # :nodoc:
+    base.send(:include, InstanceMethods)
+  end
+  module InstanceMethods
+    def project_membership_request(project, user, description)
+      #find all project members with manage_members role
+      mail_recipients = Array.new
+      project.members.each { |member|
+        roles = member.user.roles_for_project(project)
+        if roles.any?{|role| role.allowed_to?(:manage_members)}
+          mail_recipients << member.user.mail
+        end
+      }
+      #logger.debug('Sending project membership request to '+mail_recipients.join(','))
+      recipients mail_recipients
+      subject 'Request for Membership'
+      from user.mail
+      body :user => user,
+      :project => project,
+      :url => url_for(:controller => 'projects', :action => 'settings', :id => project.identifier),
+      :description => description
+    end
+  end
+end
