1b0142f522b2ac95eb3c616369a5e964daede519
Sascha Graef <sg@davitec.de>
2016-10-18 13:11:17 +0200

    adding route changes out of old source code changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
   get '/start/create_project', to: 'start#createProject'
 
   post '/start/create_project', to: 'start#createProject'
+
+  get 'projects/membershiprequest/:id', controller: 'projects', action: 'membershiprequest', as: :requestmembership
+
+  match 'roles/workflow/:id/:role_id/:tracker_id', :controller => 'roles', :action => 'workflow', via: :all
+
+  match 'help/:ctrl/:page', :controller => 'help', action: 'index', via: :all
+
+  get 'time_entries/report', :controller => 'time_entry_reports', :action => 'report'
+  get 'projects/:project_id/time_entries/report.:format', :controller => 'time_entry_reports', :action => 'report'
+
 end
